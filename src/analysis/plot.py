56e3e6d6426c285af3c999ef4ac0067593fa61cd
sp8cky <jh4112000@gmx.de>
2025-05-31 14:11:23 +0200

    .

--- src/analysis/plot.py
+++ src/analysis/plot.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
-import pandas as pd
-from src.analysis import dataset
 
 def plot_runtime(n_lists, time_lists, labels=None):
     if labels is None:
         labels = [None] * len(n_lists)
     for n, t, label in zip(n_lists, time_lists, labels):
-        plt.plot(n, t, marker="o", label=label)
+        t_ms = [ti * 1000 for ti in t] # calulate in milliseconds
+        plt.plot(n, t_ms, marker="o", label=label)
     plt.xlabel("n")
-    plt.ylabel("Laufzeit (s)")
+    plt.ylabel("Laufzeit (ms)")
+    plt.ysxcale("log")
     plt.title("Laufzeitverhalten der Kriterien")
     plt.legend()
     plt.grid(True)
